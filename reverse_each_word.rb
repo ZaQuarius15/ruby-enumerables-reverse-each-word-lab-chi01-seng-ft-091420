d13fffbe967be8840ad401db59ed1f4c44840611
Zack Carlson <jzcarlson014@gmail.com>
2020-08-27 12:49:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,10 @@
 def reverse_each_word(sen)
   senarr = sen.split
-  count = 0
   senarr.collect do |word|
     wordarr = word.split
     nwa = wordarr.reverse
     new_word = nwa.join
     new_str = new_str + " " + new_word
-    count += 1
   end
 end
 
