d4f1353311f8c843f8f8a7f7934e3a22ea8d11f0
Zack Carlson <jzcarlson014@gmail.com>
2020-08-27 12:59:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,9 @@ def reverse_each_word(sen)
   rev_sen = ''
   senarr = sen.split
   senarr.collect do |word|
-    word_arr = word.split
-    new_word = word_arr.reverse 
-    rev_word = new_word.join
-    rev_sen = rev_sen + " " + rev_word
+    word.reverse 
   end
-  rev_sen.strip
+  senarr.join
 end
 
 
